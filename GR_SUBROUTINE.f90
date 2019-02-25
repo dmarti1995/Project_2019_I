649efb0e6ff10b8f395c688a96c7561df0615324
Joan Giralt Ibañez <joangi96@gmail.com>
2019-02-25 23:18:03 +0100

    Versiones finales testeadas de las subrutinas de calculo de g(r) GR_SUBROUTINE.f90 y del mean square displacement MEANSQUAREDISP_SUBROUTINE.f90

--- GR_SUBROUTINE.f90
+++ GR_SUBROUTINE.f90
@@ -1,18 +1,20 @@
 !------------------------------------------------------------------------
-subroutine gr(N,L,COORD,g,density)
+subroutine gr(density,L,R,ncajas,g)
+real(8),parameter:: pi = acos(-1.0d0)
+real(8),parameter:: con = (4.0d0/3.0d0)*pi
+real(8),intent(in):: density,L
+real(8),intent(in):: R(:,:)
+integer,intent(in):: ncajas
 integer:: N
-real(8):: density
-real(8):: L
-real(8):: COORD(3,N)
 real(8):: xmin, xmax,interv,contint
 integer:: Histogram(ncajas),caja
-real(8):: g(ncajas)
+real(8),intent(out):: g(ncajas)
 integer:: Nx,Ny,Nz
 integer:: i,j
-real(8):: r(3),rL(3)
-real(8):: R2,R2_caja
-integer:: cont
+real(8):: rad(3),rL(3)
+real(8):: R2
 
+N = size(R,1)
 
 xmin = 0.0d0
 xmax = 2.0D0*L
@@ -20,15 +22,14 @@ interv = (xmax-xmin)/dble(ncajas)
 
 g = 0.0d0
 Histogram = 0
-cont = 0
 
-do i = 1,N
+do i = 1,N-1
 	do j = i+1,N
-		r = (COORD(:,j)-COORD(:,i))
+		rad = (R(j,:)-R(i,:))
 		do Nx = -1,1
 			do Ny = -1,1
 				do Nz = -1,1
-					rL = r + L*(/Nx,Ny,Nz/)
+					rL = rad + L*(/Nx,Ny,Nz/)
 					R2 = sqrt(sum(rL**2))
 					caja = int(r2/interv) + 1
 					if (caja<0) then
@@ -37,7 +38,6 @@ do i = 1,N
 						cycle
 					else
 					Histogram(caja) = Histogram(caja) + 1
-					cont = cont + 1
 					endif
 				enddo
 			enddo
--- MEANSQUAREDISP_SUBROUTINE.f90
+++ MEANSQUAREDISP_SUBROUTINE.f90
@@ -1,19 +1,20 @@
 !------------------------------------------------------------------------
-subroutine MSDISPLACEMENT(N,RN,Rfixed,meansquaredisp,L)
-integer:: N
-real(8):: RN(3,N),Rfixed(3,N)
-real(8):: rj_imatge(3)
-real(8):: suma,meansquaredisp
-real(8):: L
+subroutine MSDISPLACEMENT(Rcurrent,Rfixed,L,meansquaredisp)
+real(8),intent(in):: Rcurrent(:,:),Rfixed(:,:)
+real(8),intent(in):: L
+real(8),intent(out):: meansquaredisp
+real(8):: R(3)
+real(8):: suma
 integer:: i
-real(8):: D2
+
 
 suma = 0.0D0
-do i = 1,N
-	call PBCD(RN(:,i),Rfixed(:,i),L,D2,rj_imatge)
-	suma = suma + D2
+do i = 1,size(Rcurrent,1)
+	R = Rcurrent(i,:)-Rfixed(i,:)
+	call PBC(1,L,R) 
+	suma = suma + (sum(R**2))
 enddo
 
-meansquaredisp = suma/dble(N)
+meansquaredisp = suma/dble(size(Rcurrent,1))
 end subroutine MSDISPLACEMENT
 !------------------------------------------------------------------------
