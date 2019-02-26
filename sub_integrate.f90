a56ddd2038b6e5607f80c30000bfca3014e81ffa
Jon Lopez <jonlop96@gmail.com>
2019-02-26 12:11:49 +0100

    Update integration termostato+barostato

--- sub_integrate.f90
+++ sub_integrate.f90
@@ -1,57 +1,36 @@
-subroutine v_verlet_step (r, v, t, dt, Rcut, L, F)
+subroutine leap_frog (r, v, vinf, vsup, t, F, Ppot, dt, tau, Rcut, L, Text, Pext)
 implicit none
-real, intent(inout) :: r(:,:), v(:,:), t
-real, intent(in)    :: dt, Rcut, L
-real, intent(out)   :: F(:,:)
-integer             :: N
+real, intent(inout) :: r(:,:), vinf(:,:), vsup(:,:), v(:,:), t, L
+real, intent(in)    :: F(:,:), Ppot, dt, Rcut, tau, Text, Pext
+real                :: Ekin, Pcalc, Tcalc, lambda, mu
+integer             :: i, j, N
 
 N = size(r,1)
-call force (N, L, Rcut, r, F)
-r = r + v*dt + 0.5*F*dt**2
-v = v + 0.5*F*dt
 
+! TERMOSTATO
+Ekin   = 0.5 * sum(vinf**2)
+Tcalc  = 2.0 * Ekin / (3.0*N)
+lambda = sqrt(1.0 + dt/tau * (Text/Tcalc - 1.0))
+
+! BAROSTATO
+Pcalc = Ppot + 2.0*Ekin/(3.0 * L**3)
+mu    = (1.0 + dt/tau*(Pcalc-Pext))**(1.0/3.0)
+
+! Leap frog + reescalados
+vinf = vsup
+vsup = lambda * (vinf + F*dt)
+r    = r + vsup*dt
+
+! Reescalar r y L
+L = L * mu
+r = r * mu
+
+! v(t)
+v = 0.5 * (vinf + vsup)
+
+! Condiciones periodicas
 call PBC (N, L, r)
 
-call force (N, L, Rcut, r, F)
-v = v + 0.5*F*dt
+t = t + dt
 
-end subroutine v_verlet_step
-
-
-subroutine thermostat (v, nu, temperature)
-implicit none
-real, intent(inout) :: v(:,:)
-real, intent(in)    :: nu, temperature
-real                :: u, z1, z2
-integer             :: i, j
-
-do i = 1, size(v,1)
-  call random_number(u)
-  if (u < nu) then
-    call normal_distr(0.0, sqrt(temperature), z1, z2)
-    v(i,1) = z1
-    v(i,2) = z2
-    call normal_distr(0.0, sqrt(temperature), z1, z2)
-    v(i,3) = z1
-  endif
-enddo
-
-end subroutine thermostat
-
-
-subroutine normal_distr(mu, sigma, z1, z2)
-implicit none 
-real, intent(in)  :: mu, sigma
-real, intent(out) :: z1, z2
-real, parameter   :: pi = acos(-1.0)
-real              :: x, y
-
-call random_number(x)
-call random_number(y)
-z1 = sqrt(-2.0 * log(x)) * cos(2.0 * pi * y)
-z2 = sqrt(-2.0 * log(x)) * sin(2.0 * pi * y)
-z1 = z1 * sigma + mu
-z2 = z2 * sigma + mu
-
-end subroutine normal_distr
-
+end subroutine leap_frog
