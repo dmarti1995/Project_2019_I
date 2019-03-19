9545886132f822d737dcf2a4da353e5c79e88c20
Didac Marti <didac.marti.1995@gmail.com>
2019-03-19 12:38:43 +0100

    Updated with new nomenclature

--- CODIGO_PARALELO/sub_periodic_paralel.f90
+++ CODIGO_PARALELO/sub_periodic_paralel.f90
@@ -1,19 +1,13 @@
-SUBROUTINE PBC(nat,L,X,numproc,taskid) !Periodic boundary conditions
+SUBROUTINE PBC(nat,L,X,table,nproc,taskid) !Periodic boundary conditions
 implicit none
-integer :: nat,ii,jj,numproc,taskid,partition
+integer :: nat,ii,jj,numproc,taskid,
+integer :: table(0:nproc-1,2),imin,imax
 real    :: L, X(nat,3)
-partition=nat/numproc
-if (taskid.lt.(numproc-1)) then
-   do ii=taskid*partition+1,partition*(taskid+1)
-      do jj=1,3
-         X(ii,jj)=X(ii,jj)-floor(X(ii,jj)/L)*L
-      enddo
+imin=table(taskid,1)
+imax=table(taskid,2)
+do ii=imin,imax
+   do jj=1,3
+      X(ii,jj)=X(ii,jj)-floor(X(ii,jj)/L)*L
    enddo
-else
-   do ii=taskid*partition+1,nat
-      do jj=1,3
-         X(ii,jj)=X(ii,jj)-floor(X(ii,jj)/L)*L
-      enddo
-   enddo
-endif
+enddo
 END SUBROUTINE
