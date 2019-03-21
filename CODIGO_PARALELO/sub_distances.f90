e36a77886106fe36d0a5d236f9d8da08219d15ba
Joan Giralt Ibañez <joangi96@gmail.com>
2019-03-21 14:50:21 +0100

    distance subroutine mejorada

--- CODIGO_PARALELO/sub_distances.f90
+++ CODIGO_PARALELO/sub_distances.f90
@@ -1,23 +1,28 @@
-subroutine dist(N,dim,L,COORD,R2ij)
+subroutine dist(N,dim,L,COORD,R2ij,MATRR2ij)
 !N(in); numero de part
 !dim(in): dimension sistema
 !L(in): system length
 integer,intent(in):: N,dim
 real,intent(in):: L
 real,intent(in):: COORD(N,dim)
-real,intent(out):: R2ij(N,N)
+real,intent(out):: R2ij(N*(N-1)/2),MATRR2ij(i,j)
 real:: Rij(dim)
-integer:: i,j
+integer:: i,j,cont
 
 
 R2ij = 0.0
+MATRR2ij = 0.0
+
+cont = 1
 
 do i = 1,N-1
 	do j = i+1,N
 		Rij = (COORD(j,:)-COORD(i,:)) 
 		Rij = Rij - nint(Rij/L)*L
-		R2ij(i,j) = (sum(Rij**2))
-		R2ij(j,i) = R2ij(i,j)
+		R2ij(cont) = (sum(Rij**2))
+		MATRR2ij(i,j) = R2ij(cont)
+		MATRR2ij(j,i) = R2ij(cont)
+		cont = cont + 1
 	enddo
 enddo
 
