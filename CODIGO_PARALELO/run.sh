9ac686a75207a0c2f61ae3ccdd9588c5ff9a6e87
Didac Marti <didac.marti.1995@gmail.com>
2019-03-26 16:00:05 +0100

    pbc paralel working well

--- CODIGO_PARALELO/run.sh
+++ CODIGO_PARALELO/run.sh
@@ -1,11 +1,11 @@
 gfortran -c sub_distances.f90
 gfortran -c sub_fuerzas.f90
 gfortran -c sub_initialize_r.f90
-gfortran -c sub_periodic.f90
+mpif90 -c sub_periodic_paralel.f90
 mpif90 -c sub_integrate.f90
 mpif90 -c sub_meansquare.f90
 mpif90 -c sub_gr.f90
 mpif90 -c sub_initialize_v.f90
 mpif90 -c main.f90
 
-mpif90 sub_integrate.o sub_periodic.o sub_fuerzas.o sub_initialize_r.o sub_initialize_v.o sub_gr.o sub_meansquare.o main.o
+mpif90 sub_integrate.o sub_periodic_paralel.o sub_fuerzas.o sub_initialize_r.o sub_initialize_v.o sub_gr.o sub_meansquare.o main.o
