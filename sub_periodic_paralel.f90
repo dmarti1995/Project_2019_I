0715ecb47bb6f5cd53aad6645734b167e66e61d2
Didac Marti <didac.marti.1995@gmail.com>
2019-03-26 14:26:02 +0100

    definitive veresion of pbc paralel

---
+++ sub_periodic_paralel.f90
@@ -0,0 +1,22 @@
+SUBROUTINE PBC(nat,L,X,numproc,taskid,counts,displs,maxlength,&
+           ierror) !Periodic boundary conditions
+use mpi
+implicit none
+integer :: nat,ii,jj,numproc,taskid,kk,counts(numproc)
+integer :: displs(numproc),maxlength,ierror,my_N_elem
+real    :: L, X(nat,3),local_X(maxlength)
+my_N_elem=counts(taskid+1)
+kk=1+(displs(taskid+1)+1)/nat
+jj=displs(taskid+1)+1-(kk-1)*nat
+do ii=1,my_N_elem
+      local_X(ii)=X(jj,kk)-floor(X(jj,kk)/L)*L
+      if (jj==nat) then
+         jj=0
+         kk=kk+1
+      endif
+      jj=jj+1
+enddo
+call MPI_ALLGATHERV (local_X,my_N_elem , MPI_REAL,  &
+                     X, counts, displs, MPI_REAL,   &
+                     MPI_COMM_WORLD, ierror)
+END SUBROUTINE
